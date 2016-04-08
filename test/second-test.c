96fdb6e2a7d6097d7788656c01eca1e91bf3a429
Suriya Soutmun <deu@odd-e.co.th>
2016-04-08 19:24:20 +0700

    change Makefile to use aruments and add second-test for sample

---
+++ test/second-test.c
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+#include <testing.h>
+
+TEST(second, expect_int_eq) { EXPECT_INT_EQ(1, 1); }
+
+TEST(second, expect_str_eq) { EXPECT_STR_EQ("HELLO", "HELLO"); }
+
+TEST(second, expect_false) {
+  EXPECT_FALSE(false);
+  EXPECT_FALSE(0);
+  EXPECT_FALSE(1 == 0);
+}
+
+TEST(second, expect_true) {
+  EXPECT_TRUE(true);
+  EXPECT_TRUE(1);
+  EXPECT_TRUE(1 == 1);
+}
+
+START_RUN_TESTS {
+  TEST_ENTRY(second, expect_int_eq)
+  TEST_ENTRY(second, expect_str_eq)
+  TEST_ENTRY(second, expect_false)
+  TEST_ENTRY(second, expect_true)
+}
+END_RUN_TESTS
