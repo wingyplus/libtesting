669a0af68022335586bf81c3e4d822d697c9e7fa
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-04-15 23:21:54 +0700

    Validate type before assert

--- include/testing.h
+++ include/testing.h
@@ -1,6 +1,8 @@
 #ifndef TESTING_H_
 #define TESTING_H_
 
+#include <stdbool.h>
+
 #include <setjmp.h>
 #include <stdarg.h>
 #include <stddef.h>
@@ -20,12 +22,28 @@
 #define TEST_ENTRY(subject, name) cmocka_unit_test(test_##subject##_##name),
 
 #define EXPECT_STR_EQ(actual, expected) \
-  assert_string_equal((actual), (expected))
+  {                                     \
+    const char *a = actual;             \
+    const char *e = expected;           \
+    assert_string_equal((a), (e));      \
+  }
 
-#define EXPECT_INT_EQ(actual, expected) assert_int_equal((actual), (expected))
+#define EXPECT_INT_EQ(actual, expected) \
+  {                                     \
+    int a = actual, e = expected;       \
+    assert_int_equal((a), (e));         \
+  }
 
-#define EXPECT_TRUE(expr) assert_true((expr))
+#define EXPECT_TRUE(expr) \
+  {                       \
+    bool e = expr;        \
+    assert_true((e));     \
+  }
 
-#define EXPECT_FALSE(expr) assert_false((expr))
+#define EXPECT_FALSE(expr) \
+  {                        \
+    bool e = expr;         \
+    assert_false((e));     \
+  }
 
 #endif /* TESTING_H_ */
